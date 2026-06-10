87c28bb3109220cf5f21aafc34d8ed40ac83ec02
neazahsan-himel <neazahsan.ac@gmail.com>
2026-06-10 13:53:20 +0600

    Initial commit

---
+++ src/main/java/com/neaz/logsummarizer/LogSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.neaz.logsummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSummarizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogSummarizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neaz/logsummarizer/LogSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neaz.logsummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogSummarizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
